ff9acd7d6da455806566e515f6cc0b8ca7c13e3c
Sean McPherson <git@seanmcp.com>
2023-01-12 10:46:28 -0500

    task: build source zip for reviewers

--- build.sh
+++ build.sh
@@ -3,3 +3,5 @@
 version=$(cat ./manifest.json | node -pe 'JSON.parse(fs.readFileSync(0)).version')
 
 zip -r build-v$version.zip ./manifest.json ./icon.png ./dist/
+
+zip -r source-v$version.zip ./manifest.json ./package.json ./icon.png ./src/
