11131032c19ac235fed6c087cda556b89406f0bb
Sean McPherson <git@seanmcp.com>
2023-01-13 13:17:28 -0500

    task: rename, document package script

--- pack.sh
+++ pack.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 
+# Remove old builds
+rm -f {build,source}-v*.zip
+
+# Build the extension
+npm run build
+
+# Read version from manifest.json
 version=$(cat ./manifest.json | node -pe 'JSON.parse(fs.readFileSync(0)).version')
 
+# Generate build-v%.%.%.zip (for submission)
 zip -r build-v$version.zip ./manifest.json ./icon.png ./dist/
 
+# Generate source-v%.%.%.zip (for review)
 zip -r source-v$version.zip ./manifest.json ./package.json ./icon.png ./src/
